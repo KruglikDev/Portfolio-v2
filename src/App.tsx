31ac2f13e5042ff5ad79ae12f8f38fe54e3b9d42
Andrey Kruglik <isekaiguy@protonmail.com>
2022-09-18 17:13:11 +0300

    Spline model added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import Spline from '@splinetool/react-spline';
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hello
-      </header>
+    <div className="flex w-screen h-screen min-h-screen flex-col items-center justify-center relative bg-primary pb-20">
+        {/*<div className="relative" id='home'>*/}
+            <Spline scene="https://prod.spline.design/WqAqUDqtpxB9lX9h/scene.splinecode" />
+        {/*</div>*/}
     </div>
   );
 }
