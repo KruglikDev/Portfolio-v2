043a4a64d11ddb500c1555d08de6731f420704d3
KruglikDev <isekaiguy@protonmail.com>
2022-11-06 13:01:15 +0300

    Removed animation in projects section, changed photo

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,49 +1,38 @@
 import {projects} from '../utils/data.js';
-import {AnimatePresence, motion} from "framer-motion";
 
 const Projects = () => {
     return (
-        <AnimatePresence>
-            <motion.section
-                initial="hidden"
-                whileInView="visible"
-                viewport={{once: true}}
-                transition={{type: "spring"}}
-                variants={{
-                    visible: {opacity: 1, scale: 1},
-                    hidden: {opacity: 0, scale: 0}
-                }}
-                className="flex flex-wrap items-center justify-evenly my-24 gap-4 px-10 max-w-7xl"
-                id="projects"
-            >
-                {projects && projects.map(el => (
-                    <a href={el.link} key={el.id}>
-                        <div
-                            className="border border-zinc-800 rounded-md p-2 min-w-[275px] md:max-w-[360px] hover:border-zinc-600 duration-100 ease-in-out"
-                        >
-                            <p className="text-lg text-textBase font-medium uppercase">
-                                {el.name.length > 24 ? `${el.name.slice(0, 24)}...` : el.name}
+        <section
+            className="flex flex-wrap items-center justify-evenly my-24 gap-4 px-10 max-w-7xl"
+            id="projects"
+        >
+            {projects && projects.map(el => (
+                <a href={el.link} key={el.id}>
+                    <div
+                        className="border border-zinc-800 rounded-md p-2 min-w-[275px] md:max-w-[360px] hover:border-zinc-600 duration-100 ease-in-out"
+                    >
+                        <p className="text-lg text-textBase font-medium uppercase">
+                            {el.name.length > 24 ? `${el.name.slice(0, 24)}...` : el.name}
+                        </p>
+
+                        <img
+                            src={el.imageSrc}
+                            className="w-full h-full object-cover rounded-md my-4"
+                            alt=""
+                        />
+
+                        <div className="flex flex-1 items-center justify-between">
+                            <p className="text-lg text-gray-300">
+                                Technologies
+                                <div className="flex gap-2">
+                                    {el.icons?.map((icon, i) => <div key={i}>{icon}</div>)}
+                                </div>
                             </p>
-
-                            <img
-                                src={el.imageSrc}
-                                className="w-full h-full object-cover rounded-md my-4"
-                                alt=""
-                            />
-
-                            <div className="flex flex-1 items-center justify-between">
-                                <p className="text-lg text-gray-300">
-                                    Technologies
-                                    <div className="flex gap-2">
-                                        {el.icons?.map((icon, i) => <div key={i}>{icon}</div>)}
-                                    </div>
-                                </p>
-                            </div>
                         </div>
-                    </a>
-                ))}
-            </motion.section>
-        </AnimatePresence>
+                    </div>
+                </a>
+            ))}
+        </section>
     )
 }
 
